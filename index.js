37441267de8a97493abe4f7b5c3ea5a2a1669f78
Julian Waller <git@julusian.co.uk>
2022-02-02 16:44:10 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -542,7 +542,7 @@ instance.prototype.destroy = function() {
 instance.prototype.actions = function(system) {
 	let self = this;
 
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 
 		'set_intensity': {
 			label: 'Set Playback Intensity',
